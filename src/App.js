b0858f0a5fe0cb0d4f12039b2bd7560c6b20cc91
alisher2007feg <113277011+alisher2007feg@users.noreply.github.com>
2023-05-06 05:12:31 +0500

    origin main

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import Todolist from './Todolist/Todolist';
+import RestCountrist from './RestCountrist/RestCountrist';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Todolist/>
+        <RestCountrist/> 
+      </header>
+      
+    </div>
+  
+  );
+}
+
+export default App;
